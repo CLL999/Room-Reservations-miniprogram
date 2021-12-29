91db943132c25143a42a3459c37c99deb52ee493
CLL999 <cirnoqvq@163.com>
2021-12-29 14:42:31 +0800

    样式修改

--- src/components/RoomCard/index.tsx
+++ src/components/RoomCard/index.tsx
@@ -12,12 +12,14 @@ export default function RoomCard(props) {
     }
 
     return (
-        <View className='w-70 h-78 bg-white shadow-2xl rounded-3xl mx-12 inline-block relative'>
-            <Image src={pot} className='absolute h-52 w-52 -top-8 left-26 overflow-visible z-50'></Image>
-            <Text className=' text-4xl font-bold absolute top-24 left-5'>活动</Text>
-            <Text className=' text-4xl font-bold absolute top-34 left-5'>中心</Text>
-            <Text className=' text-7xl font-bold absolute top-43 left-9 text-black text-opacity-16 truncate w-65'>{props.name}</Text>
-            <Button className=' rounded-full py-0 px-4 absolute primarybutton bottom-2 right-3 w-28 h-12 text-center font-bold border-separate' onClick={toRoomDetail}>了解更多</Button>
+        <View className='h-78 w-screen inline-block '>
+            <View className='w-62 h-70 bg-white shadow-2xl rounded-3xl mx-auto relative'>
+                <Image src={pot} className='absolute h-44 w-44 -top-8 left-29 overflow-visible z-50'></Image>
+                <Text className=' text-4xl font-bold absolute top-14 left-5'>活动</Text>
+                <Text className=' text-4xl font-bold absolute top-24 left-5'>中心</Text>
+                <Text className=' text-6xl font-bold absolute top-38 left-4 text-black text-opacity-16 truncate w-65'>{props.name}</Text>
+                <Button className=' rounded-full py-0 px-4 absolute primarybutton bottom-2 right-3 w-28 h-12 text-center font-bold border-separate' onClick={toRoomDetail}>了解更多</Button>
+            </View>
         </View>
     )
 }
